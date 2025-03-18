fd3ee6672ae0ea553f1e6009f38bdb605261151b
sen46Sko <visosensey@gmail.com>
2025-03-18 19:47:08 +0200

    feat: yearly&monthly stats tables

---
+++ database/migrations/2025_03_18_173515_create_year_stats_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateYearStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('year_stats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('year');
+            $table->integer('subscription_counter');
+            $table->integer('trial_counter');
+            $table->integer('cancle_counter');
+            $table->integer('monthly_plan');
+            $table->integer('yearly_plan');
+            $table->integer('total_users');
+            $table->integer('signups');
+            $table->integer('delete_account_counter');
+            $table->integer('resolved_tickets');
+            $table->integer('ticket_created');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('year_stats');
+    }
+}
