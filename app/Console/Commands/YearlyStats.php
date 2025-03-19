fbebc097c9960888f88fcf04f230a7c122689158
sen46Sko <visosensey@gmail.com>
2025-03-19 11:17:12 +0200

    feat: yearly crons

--- app/Console/Commands/YearlyStats.php
+++ app/Console/Commands/YearlyStats.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\YearStats;
 
 class YearlyStats extends Command
 {
@@ -37,6 +40,19 @@ class YearlyStats extends Command
      */
     public function handle()
     {
+        $year = Carbon::now()->year;
+
+        $totalUsers = User::count();
+
+        $exists = YearStats::where('year', $year)->exists();
+
+        if(!$exists) {
+            YearStats::create([
+                'year' => $year,
+                'total_users' => $totalUsers,
+            ]);
+        }
+
         return 0;
     }
 }
---
+++ app/Models/YearStats.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class YearStats extends Model
+{
+    use HasFactory;
+
+    protected $table = "year_stats";
+
+    protected $fillable = [
+        'year', 
+        'total_users',
+        'subscription_counter',
+        'trial_counter',
+        'cancle_counter',
+        'monthly_plan',
+        'yearly_plan',
+        'signups',
+        'delete_account_counter',
+        'resolved_tickets',
+        'ticket_created'
+    ];
+}
